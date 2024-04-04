4e982d69462d0691551169ce4b9f76144a29fe22
Mohamad Fadel <fadelalatas81@gmail.com>
2024-04-05 00:29:11 +0700

    test push

---
+++ src/app/components/ProjectCard.tsx
@@ -0,0 +1,51 @@
+"use client"
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
+
+interface Props {
+    image: string;
+    title: string;
+    text: string;
+}
+
+const ProjectCard = ({ image, title, text }: Props) => {
+    const [isFlipped, setIsFlipped] = useState(false)
+    const [isAnimating, setIsAnimating] = useState(false)
+
+    function handleFlip() {
+        if (!isAnimating) {
+            setIsFlipped(!isFlipped)
+            setIsAnimating(true)
+        }
+    }
+    return (
+        <div>
+            <div className='flex flex-col gap-[1rem] py-3 z-[30]'>
+                <h1 className='text-whote text-md font-semibold'>{title}</h1>
+                <p className='text-gray-200 text-[14px]'>
+                    {text}
+                </p>
+            </div>
+            <div
+                onClick={handleFlip}
+                className='w-[450px] h-[280px] rounded-md cursor-pointer'>
+                <motion.div
+                    className='flip-card-inner w-full h-full'
+                    initial={false}
+                    animate={{ rotateY: isFlipped ? 180 : 360 }}
+                    transition={{ duration: 0.6, animationDirection: 'normal' }}
+                    onAnimationComplete={() => setIsAnimating(false)}
+                >
+                    <div
+                        style={{ backgroundImage: `url(${image})` }}
+                        className='w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p-4'>
+
+                    </div>
+                </motion.div>
+            </div>
+        </div>
+
+    )
+}
+
+export default ProjectCard
