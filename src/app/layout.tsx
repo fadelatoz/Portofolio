b016295b78f4931c58466e5acedd5a63f2a48ccd
Mohamad Fadel <fadelalatas81@gmail.com>
2024-04-06 13:50:09 +0700

    test analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Navbar from './components/Navbar'
 import Navigation from './components/Navigation'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <SpeedInsights/>
+        <Analytics/>
         <Navbar />
         {children}
         <Navigation />
