3bd06e575d529ab722bfb22c5928333b579577a2
Mohamad Fadel <fadelalatas81@gmail.com>
2024-05-11 15:27:18 +0700

    update skills

--- src/app/my-skills/page.tsx
+++ src/app/my-skills/page.tsx
@@ -27,7 +27,7 @@ const PageSkills = () => {
       <div className=" flex items-center justify-center">
         <Skills />
       </div>
-      <div className="grid grid-cols-1 p-10 gap-1 md:grid-cols-4 md:gap-4 md:ml-6 max-h-[500px] overflow-y-auto">
+      <div className="grid grid-cols-1 p-10 gap-1 md:grid-cols-4 md:gap-4 md:ml-6 max-h-[300px] overflow-y-auto">
         {SkillsCard.map((v, k) => (
           <div key={k} className="place-items-center p-5 bg-gray-100 dark:bg-gray-900 rounded-md active:bg-secondary md:hover:bg-green-700 transition cursor-pointer hover:scale-95">
             <p className="text-2xl text-black dark:text-white md:hover:text-white">{v.title}</p>
